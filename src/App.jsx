bb29437959a20ed7991f22c6d25e15c6689660ad
igorrm23 <igormachadofire14@gmail.com>
2026-02-08 17:04:07 -0300

    adicionando container

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,17 @@ function App() {
   return (
     <>
       <div className='container'>
-        <h1>Hello World</h1>
-      </div>
+        <h1>WISH LIST</h1>
+        <div className='wish-list'>
+           <form className='list-form'>
+             <input className='list-input' type="text" placeholder='Add a new wish' />
+             <button className='list-button'>Add</button>
+           </form>
+           <div className='list-container'>
+            <h1>1</h1>
+           </div>
+          </div>
+        </div>
     </>
   )
 }
